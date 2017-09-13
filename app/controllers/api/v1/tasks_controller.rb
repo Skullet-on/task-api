06675c77a6c3082eba91ff9e970caaeab2612a83
Eugene <skullet-on@mail.ru>
2017-09-13 15:23:45 +0300

    Add Serializer

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,18 @@
+module Api
+	module V1
+		class TasksController < ApplicationController
+			def index
+			end
+
+			def show
+			end
+
+			def destroy
+			end
+
+			private
+			def set_task
+			end
+		end
+	end
+end
